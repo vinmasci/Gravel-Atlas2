1e0761faec65869c4023d0d1c1da10b851609036
vinmasci <vincentmasci@icloud.com>
2024-11-08 21:04:06 +1100

    Create IndexPage.js

---
+++ pages/IndexPage.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const IndexPage = () => {
+  return (
+    <>
+      <!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charSet="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <title>Mapbox Project</title>
+          <link href="https://api.mapbox.com/mapbox-gl-js/v2.12.0/mapbox-gl.css" rel="stylesheet" />
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
+          <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet" />
+        </head>
+        <body>
+          {/* Your HTML content here */}
+          <script src="https://api.mapbox.com/mapbox-gl-js/v2.12.0/mapbox-gl.js"></script>
+          <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+          <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+        </body>
+      </html>
+    </>
+  );
+};
+
+export default IndexPage;
