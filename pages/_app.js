11a402e4726a8c7f7dcb4e27aed516075b986726
vinmasci <vincentmasci@icloud.com>
2024-11-08 18:13:30 +1100

    auth0

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import { UserProvider } from '@auth0/nextjs-auth0';
+import '../styles/globals.css';  // Add this line
 
 export default function App({ Component, pageProps }) {
   return (
--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,7 @@ import { useEffect } from 'react';
 import Head from 'next/head';
 
 export default function Home() {
-  // Debug: Let's add some console logs to check if the component is rendering
-  console.log("Home component rendering");
-
   useEffect(() => {
-    console.log("useEffect running");
     if (typeof window !== 'undefined') {
       require('../public/js/map.js');
       require('../public/js/routes.js');
@@ -20,7 +16,6 @@ export default function Home() {
   }, []);
 
   const { user, isLoading } = useUser();
-  console.log("Auth state:", { user, isLoading }); // Debug: Check auth state
 
   return (
     <>
@@ -31,29 +26,14 @@ export default function Home() {
         <link href="https://api.mapbox.com/mapbox-gl-js/v2.12.0/mapbox-gl.css" rel="stylesheet" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
         <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet" />
-        <script src="https://kit.fontawesome.com/b02e210188.js" crossOrigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/compressorjs/1.0.7/compressor.min.js"></script>
       </Head>
 
-      {/* Debug: Add a test element */}
-      <div style={{
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        backgroundColor: 'red',
-        zIndex: 9999,
-        padding: '5px'
-      }}>
-        Test Element
-      </div>
-
       <AuthButton />
-
-      <main>
-        <div id="map"></div>
-        <div id="dropArea"></div>
-        {/* Rest of your HTML content */}
-      </main>
+      
+      {/* Load the HTML content */}
+      <div dangerouslySetInnerHTML={{ 
+        __html: require('fs').readFileSync('./index.html', 'utf8') 
+      }} />
 
       {/* External Scripts */}
       <script src="https://api.mapbox.com/mapbox-gl-js/v2.12.0/mapbox-gl.js"></script>
