cfefa3dc2f882f7650fd997a8154ddf0d85a759b
vinmasci <vincentmasci@icloud.com>
2024-12-04 22:10:25 +1100

    Update markers.js

--- public/js/markers.js
+++ public/js/markers.js
@@ -25,19 +25,21 @@ function initPOILayers() {
                     'visibility': 'none'
                 },
                 'paint': {
-                    'circle-radius': 8,
+                    'circle-radius': 10,
                     'circle-color': [
                         'match',
                         ['get', 'amenity_type'],
-                        'toilets', '#e74c3c',
-                        'fuel', '#f1c40f',
-                        'cafe', '#9b59b6',
+                        'toilets', '#e74c3c',      // Red for toilets
+                        'fuel', '#f1c40f',         // Yellow for service stations
+                        'cafe', '#9b59b6',         // Purple for cafes
+                        'drinking_water', '#3498db', // Blue for drinking water
                         ['match', 
                             ['get', 'tourism'],
-                            'camp_site', '#2ecc71',
+                            'camp_site', '#2ecc71',  // Green for campsites
                             ['match',
                                 ['get', 'shop'],
-                                'bicycle', '#3498db',
+                                'bicycle', '#3498db',    // Blue for bike shops
+                                'supermarket', '#e67e22', // Orange for supermarkets
                                 'transparent'
                             ]
                         ]
@@ -48,8 +50,10 @@ function initPOILayers() {
                             ['==', ['get', 'amenity_type'], 'toilets'],
                             ['==', ['get', 'amenity_type'], 'fuel'],
                             ['==', ['get', 'amenity_type'], 'cafe'],
+                            ['==', ['get', 'amenity_type'], 'drinking_water'],
                             ['==', ['get', 'tourism'], 'camp_site'],
-                            ['==', ['get', 'shop'], 'bicycle']
+                            ['==', ['get', 'shop'], 'bicycle'],
+                            ['==', ['get', 'shop'], 'supermarket']
                         ],
                         0.8,
                         0
@@ -60,8 +64,10 @@ function initPOILayers() {
                             ['==', ['get', 'amenity_type'], 'toilets'],
                             ['==', ['get', 'amenity_type'], 'fuel'],
                             ['==', ['get', 'amenity_type'], 'cafe'],
+                            ['==', ['get', 'amenity_type'], 'drinking_water'],
                             ['==', ['get', 'tourism'], 'camp_site'],
-                            ['==', ['get', 'shop'], 'bicycle']
+                            ['==', ['get', 'shop'], 'bicycle'],
+                            ['==', ['get', 'shop'], 'supermarket']
                         ],
                         2,
                         0
@@ -70,7 +76,16 @@ function initPOILayers() {
                 }
             });
 
-            // Add icons layer on top
+            // Create Font Awesome icons first
+            addFontAwesomeIcon('fa-restroom');      // Toilets
+            addFontAwesomeIcon('fa-gas-pump');      // Service stations
+            addFontAwesomeIcon('fa-mug-hot');       // Cafes
+            addFontAwesomeIcon('fa-faucet');        // Drinking water
+            addFontAwesomeIcon('fa-campground');    // Campsites
+            addFontAwesomeIcon('fa-bicycle');       // Bike shops
+            addFontAwesomeIcon('fa-shopping-cart'); // Supermarkets
+
+            // Icon layer
             map.addLayer({
                 'id': 'poi-icons',
                 'type': 'symbol',
@@ -84,30 +99,25 @@ function initPOILayers() {
                         'toilets', 'fa-restroom',
                         'fuel', 'fa-gas-pump',
                         'cafe', 'fa-mug-hot',
+                        'drinking_water', 'fa-faucet',
                         ['match',
                             ['get', 'tourism'],
                             'camp_site', 'fa-campground',
                             ['match',
                                 ['get', 'shop'],
                                 'bicycle', 'fa-bicycle',
-                                'default'
+                                'supermarket', 'fa-shopping-cart',
+                                ''
                             ]
                         ]
                     ],
                     'icon-size': 0.5,
-                    'icon-offset': [0, 0],
-                    'icon-allow-overlap': true
+                    'icon-allow-overlap': true,
+                    'icon-ignore-placement': true
                 }
             });
 
-            // Add Font Awesome icons
-            addFontAwesomeIcon('fa-restroom', '#ffffff');    // Toilets
-            addFontAwesomeIcon('fa-gas-pump', '#ffffff');    // Service stations
-            addFontAwesomeIcon('fa-mug-hot', '#ffffff');     // Cafes
-            addFontAwesomeIcon('fa-campground', '#ffffff');  // Campsites
-            addFontAwesomeIcon('fa-bicycle', '#ffffff');     // Bike shops
-
-            // Popup code remains the same
+            // Popup for POIs
             const popup = new mapboxgl.Popup({
                 closeButton: false,
                 closeOnClick: false
@@ -118,10 +128,9 @@ function initPOILayers() {
                 
                 const properties = e.features[0].properties;
                 let name = properties.name || 'Unnamed';
-                let type = properties.amenity_type || properties.tourism || properties.shop;
+                let type = properties.amenity_type || properties.tourism || properties.shop || '';
                 let icon = '';
 
-                // Get appropriate icon for popup
                 switch(type) {
                     case 'toilets':
                         icon = '<i class="fa-solid fa-restroom"></i>';
@@ -132,11 +141,17 @@ function initPOILayers() {
                     case 'cafe':
                         icon = '<i class="fa-solid fa-mug-hot"></i>';
                         break;
+                    case 'drinking_water':
+                        icon = '<i class="fa-solid fa-faucet"></i>';
+                        break;
                     case 'camp_site':
                         icon = '<i class="fa-solid fa-campground"></i>';
                         break;
                     case 'bicycle':
                         icon = '<i class="fa-solid fa-bicycle"></i>';
+                        break;
+                    case 'supermarket':
+                        icon = '<i class="fa-solid fa-shopping-cart"></i>';
                         break;
                 }
 
@@ -163,14 +178,14 @@ function initPOILayers() {
     }
 }
 
-// Helper function to create Font Awesome icons (white icons on colored circles)
-function addFontAwesomeIcon(iconClass, color) {
+function addFontAwesomeIcon(iconClass) {
     const canvas = document.createElement('canvas');
     canvas.width = 20;
     canvas.height = 20;
     const ctx = canvas.getContext('2d');
     
-    ctx.fillStyle = color;
+    // Background should be transparent
+    ctx.fillStyle = '#ffffff';  // White icons
     ctx.font = '14px "Font Awesome 6 Free Solid"';
     ctx.textAlign = 'center';
     ctx.textBaseline = 'middle';
@@ -180,7 +195,9 @@ function addFontAwesomeIcon(iconClass, color) {
         'fa-gas-pump': 'f52f',
         'fa-mug-hot': 'f7b6',
         'fa-campground': 'f6bb',
-        'fa-bicycle': 'f206'
+        'fa-bicycle': 'f206',
+        'fa-faucet': 'e005',
+        'fa-shopping-cart': 'f07a'
     };
     
     ctx.fillText(String.fromCharCode('0x' + iconUnicode[iconClass]), 10, 10);
@@ -189,12 +206,12 @@ function addFontAwesomeIcon(iconClass, color) {
         map.addImage(iconClass, {
             width: 20,
             height: 20,
-            data: ctx.getImageData(0, 0, 20, 20).data
+            data: ctx.getImageData(0, 0, 20, 20).data,
+            sdf: true  // Added this to help with icon rendering
         });
     }
 }
 
-// Update loadPOIMarkers to handle both layers
 async function loadPOIMarkers() {
     console.log("Loading POI markers...");
     try {
@@ -212,15 +229,16 @@ async function loadPOIMarkers() {
     }
 }
 
-// Update removePOIMarkers to handle both layers
 function removePOIMarkers() {
     console.log("Removing POI markers...");
     poiMarkers.forEach(marker => marker.remove());
     poiMarkers = [];
     
+    if (map.getLayer('poi-icons')) {
+        map.setLayoutProperty('poi-icons', 'visibility', 'none');
+    }
     if (map.getLayer('poi-layer')) {
         map.setLayoutProperty('poi-layer', 'visibility', 'none');
-        map.setLayoutProperty('poi-icons', 'visibility', 'none');
     }
 }
 
